7f01cacba376508f8e1ce41bee6c4c2971557739
dgcocit <70928781+dgcocit@users.noreply.github.com>
2020-09-07 22:01:05 -0500

    Add files via upload

---
+++ js/main.min.82df4515d2c81d29396a59208ee599d1e55a2c66ed19b9b77a47bbdcbc0f66f8.js
@@ -0,0 +1,9 @@
+(function($){var $window=$(window),$body=$('body'),$header=$('#header'),$banner=$('#banner');breakpoints({wide:('1281px','1680px'),normal:('981px','1280px'),narrow:('737px','980px'),narrower:('737px','840px'),mobile:('481px','736px'),mobilep:(null,'480px')});$window.on('load',function(){window.setTimeout(function(){$body.removeClass('is-preload');},100);});var reqURL="https://api.rss2json.com/v1/api.json?rss_url="+encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=");function loadVideo(iframe){$.getJSON(reqURL+iframe.getAttribute('cid'),function(data){var videoNumber=(iframe.getAttribute('vnum')?Number(iframe.getAttribute('vnum')):0);console.log(videoNumber);var link=data.items[videoNumber].link;id=link.substr(link.indexOf("=")+1);iframe.setAttribute("src","https://youtube.com/embed/"+id+"?controls=0&autoplay=1");});}
+var iframes=document.getElementsByClassName('latestVideoEmbed');for(var i=0,len=iframes.length;i<len;i++){loadVideo(iframes[i]);}
+$('#nav > ul').dropotron({alignment:'right'});$('<div id="navButton">'+
+'<a href="#navPanel" class="toggle"></a>'+
+'</div>').appendTo($body);$('<div id="navPanel">'+
+'<nav>'+
+$('#nav').navList()+
+'</nav>'+
+'</div>').appendTo($body).panel({delay:500,hideOnClick:true,hideOnSwipe:true,resetScroll:true,resetForms:true,side:'left',target:$body,visibleClass:'navPanel-visible'});if(!browser.mobile&&$header.hasClass('alt')&&$banner.length>0){$window.on('load',function(){$banner.scrollex({bottom:$header.outerHeight(),terminate:function(){$header.removeClass('alt');},enter:function(){$header.addClass('alt reveal');},leave:function(){$header.removeClass('alt');}});});}})(jQuery);
